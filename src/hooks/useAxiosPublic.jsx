036e6f8e913351e90bdc66dc42a608b50d1bb25e
tasinvoid <shoaibmahmud4000@gmail.com>
2025-08-06 03:33:11 +0600

    initial setup

---
+++ src/hooks/useAxiosPublic.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+import React from "react";
+
+const useAxiosPublic = () => {
+  const axiosPublic = axios.create({
+    baseURL: "https://builida-server.vercel.app",
+  });
+  return axiosPublic;
+};
+
+export default useAxiosPublic;
